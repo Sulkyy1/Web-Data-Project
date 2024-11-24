76ff75cc585a212cbf7f14fa227f34f87f27b489
Sebastián Alonso Burgueño Merino <sebastian.burgueno@ug.uchile.cl>
2024-11-23 23:34:31 -0300

    Unificar comunas y regiones RDF
    Se crea un nuevo archivo data_to_rdf.py que une los códigos que había en regiones-to-rdf.py y comunas-to-rdf.py, además, crea la relación ex:isPartOf para indicar que una comuna pertenece a una región

---
+++ data_to_rdf.py
@@ -0,0 +1,59 @@
+from rdflib import Graph, Namespace, URIRef, Literal, RDF, XSD
+import csv
+import uuid
+
+# Crear un grafo RDF
+g = Graph()
+
+# Definir un namespace personalizado
+EX = Namespace("http://example.org/")
+
+# Añadir prefijos al grafo
+g.bind("ex", EX)
+
+# Cargar CSVs
+csv_regiones = './regiones.csv'     # regiones
+csv_comunas = './comunas.csv'       # comunas
+
+# Diccionario para almacenar las regiones en las que está cada comuna
+regiones = {}
+
+# Crear las Regiones
+with open(csv_regiones, mode='r', newline='') as file:
+    reader = csv.DictReader(file, delimiter=';')
+    for row in reader:
+        # Crear un sujeto único para la región utilizando el cut_region
+        id_region = row['cut_region']
+        sujeto_region = URIRef(EX[f"Region/{id_region}"])
+        
+        # Añadir tripletas al grafo para la región
+        g.add((sujeto_region, RDF.type, EX.Region))
+        g.add((sujeto_region, EX.nombre, Literal(row['region'], datatype=XSD.string)))
+        g.add((sujeto_region, EX.cut, Literal(id_region, datatype=XSD.integer)))
+        
+        # Guardar la referencia de la región con su id (cut_region)
+        regiones[id_region] = sujeto_region
+
+# Crear las Comunas
+with open(csv_comunas, mode='r', newline='') as file:
+    reader = csv.DictReader(file, delimiter=';')
+    for row in reader:
+        # Crear un sujeto único para la comuna utilizando el cut_comuna
+        id_comuna = row['cut_comuna']
+        id_region = row['cut_region']
+        sujeto_comuna = URIRef(EX[f"Comuna/{id_comuna}"])
+        
+        # Añadir tripletas al grafo para la comuna
+        g.add((sujeto_comuna, RDF.type, EX.Comuna))
+        g.add((sujeto_comuna, EX.nombre, Literal(row['comuna'], datatype=XSD.string)))
+        g.add((sujeto_comuna, EX.cut, Literal(id_comuna, datatype=XSD.integer)))
+        
+        # Relacionar la comuna con su región usando el campo 'cut_comuna' para buscar la región
+        if id_region in regiones:
+            print("aaaa")
+            g.add((sujeto_comuna, EX.isPartOf, regiones[id_region]))
+
+# Guardar en un archivo en formato Turtle
+with open("output_rdf.rdf", "w", encoding='utf-8') as f:
+    f.write(g.serialize(format='turtle'))
+
