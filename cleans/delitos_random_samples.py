11cdc063140a99381babab96eddc66b96515d74f
SebaVeraniego <alvarogarridobus@gmail.com>
2024-11-24 01:05:47 -0300

    fix: n_delitos as int and  add encoding to see accents in .rdfs files

--- cleans/delitos_random_samples.py
+++ cleans/delitos_random_samples.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # Lee todo el archivo CSV especificando el separador
-data = pd.read_csv('cead_delincuencia_chile.csv', sep=';')
+data = pd.read_csv('../data-clean/delincuencia_clean.csv', sep=';')
 
-# Toma una muestra aleatoria de 100,000 filas
+# Toma una muestra aleatoria de 1,000 filas
 data_sample = data.sample(n=1000, random_state=42)  # random_state para reproducibilidad
 
 # Guarda la muestra en un nuevo archivo CSV con ';' como separador
-data_sample.to_csv('cead_delincuencia_chile_random_sample.csv', sep=';', index=False)
+data_sample.to_csv('../data-clean/cead_delincuencia_chile_random_sample.csv', sep=';', index=False)
 
-print("Nuevo archivo creado con 100,000 filas.")
+print("Nuevo archivo creado con 1,000 filas.")
--- data-to-rdf/delincuencia_to_rdf.py
+++ data-to-rdf/delincuencia_to_rdf.py
@@ -15,8 +15,8 @@ EX = Namespace("http://example.org/")
 g.bind("ex", EX)
 
 # Cargar el CSV de delincuencia
-csv_delincuencia = './cead_delincuencia_chile_random_sample.csv'  
-with open(csv_delincuencia, mode='r', newline='') as file:
+csv_delincuencia = '../data-clean/cead_delincuencia_chile_random_sample.csv'  
+with open(csv_delincuencia, mode='r', newline='', encoding="utf-8") as file:
     reader = csv.DictReader(file, delimiter=';') 
     for row in reader:
         # Crear un sujeto único utilizando un UUID
@@ -26,12 +26,12 @@ with open(csv_delincuencia, mode='r', newline='') as file:
         #Añadir tripletas al grafo
         g.add((sujeto, RDF.type, EX.Delito))
         g.add((sujeto, EX.tipo, Literal(row['delito'], datatype=XSD.string)))
-        g.add((sujeto, EX.cantidad, Literal(row['delito_n'], datatype=XSD.integer)))
+        g.add((sujeto, EX.cantidad, Literal(row['delito_n'], datatype=XSD.int)))
         g.add((sujeto, EX.fecha, Literal(row['fecha'], datatype=XSD.date)))
         
         
 # Guardar en un archivo en formato Turtle
-with open("output_delincuencia.rdf", "w", encoding='utf-8') as f:
+with open("../output_delincuencia.rdf", "w", encoding='utf-8') as f:
     f.write(g.serialize(format='turtle'))
 
 # Mostrar el grafo RDF en pantalla
