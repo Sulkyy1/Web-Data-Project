3d086f7e3cf490e5359b3f768875722d97ed59bb
Sebastián Alonso Burgueño Merino <sebastian.burgueno@ug.uchile.cl>
2024-11-25 10:20:08 -0300

    Unir delincuencia a RDF
    Se une el código de delincuencia-to-rdf.py a data-to-rdf.py. El archivo output_rdf.rdf ahora contiene los datos de comunas, regiones y delitos.
    Se relacionan los delitos con las comunas en las que ocurrieron.

--- data-to-rdf/data_to_rdf.py
+++ data-to-rdf/data_to_rdf.py
@@ -12,11 +12,14 @@ EX = Namespace("http://example.org/")
 g.bind("ex", EX)
 
 # Cargar CSVs
-csv_regiones = './data-clean/regiones.csv'     # regiones
-csv_comunas = './data-clean/comunas.csv'       # comunas
+csv_regiones = './data-clean/regiones.csv'                                    # regiones
+csv_comunas = './data-clean/comunas.csv'                                      # comunas
+csv_delincuencia = './data-clean/cead_delincuencia_chile_random_sample.csv'   # delincuencia
 
 # Diccionario para almacenar las regiones en las que está cada comuna
 regiones = {}
+# Diccionario para almacenar las comunas en las que ocurre cada delito
+comunas ={}
 
 # Crear las Regiones
 with open(csv_regiones, mode='r', newline='', encoding="utf-8") as file:
@@ -51,6 +54,28 @@ with open(csv_comunas, mode='r', newline='', encoding="utf-8") as file:
         # Relacionar la comuna con su región usando el campo 'cut_region' para buscar la región
         if id_region in regiones:
             g.add((sujeto_comuna, EX.isPartOf, regiones[id_region]))
+
+        # Guardar la referencia de la comuna con su id (cut_comuna)
+        comunas[id_comuna] = sujeto_comuna
+
+# Crear los Delitos
+with open(csv_delincuencia, mode='r', newline='', encoding="utf-8") as file:
+    reader = csv.DictReader(file, delimiter=';') 
+    for row in reader:
+        # Crear un sujeto único utilizando un UUID
+        id_comuna = row['cut_comuna']
+        id = uuid.uuid4()
+        sujeto_delito = URIRef((EX[f"Delito/{id}"]))
+        
+        #Añadir tripletas al grafo
+        g.add((sujeto_delito, RDF.type, EX.Delito))
+        g.add((sujeto_delito, EX.tipo, Literal(row['delito'], datatype=XSD.string)))
+        g.add((sujeto_delito, EX.cantidad, Literal(row['delito_n'], datatype=XSD.int)))
+        g.add((sujeto_delito, EX.fecha, Literal(row['fecha'], datatype=XSD.date)))
+
+        # Relacionar el delito con su comuna usando el campo 'cut_comuna' para buscar la comuna
+        if id_comuna in comunas:
+            g.add((sujeto_delito, EX.happenedIn, comunas[id_comuna]))
 
 # Guardar en un archivo en formato Turtle
 with open("./output_rdf.rdf", "w", encoding='utf-8') as f:
